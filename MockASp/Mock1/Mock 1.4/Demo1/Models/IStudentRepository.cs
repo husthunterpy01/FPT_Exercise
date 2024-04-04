de7d5ac04dcc9a469048ada868bb88b090d34e23
husthunterpy01 <96186749+husthunterpy01@users.noreply.github.com>
2024-04-04 21:45:46 +0700

    Authen&Author Mock

---
+++ MockASp/Mock1/Mock 1.4/Demo1/Models/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Demo1.Entities;
+
+namespace Demo1.Models
+{
+    public interface IStudentRepository
+    {
+        Task<IEnumerable<Student>> Search(string name, Gender? gender);
+        Task<Student> GetStudent(int StudentId);
+        Task<IEnumerable<Student>> GetStudents();
+        Task<Student> GetStudentByEmail(string Email);
+        Task<Student> AddStudent(Student student);
+        Task<Student> UpdateStudent(Student student);
+        Task DeleteStudent(int StudentId);
+    }
+}
